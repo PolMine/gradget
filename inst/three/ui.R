dd9efdcd64d42009e9752e9bc9ae65f3cc111530
Andreas Blaette <andreas.blaette@uni-due.de>
2016-11-20 21:12:26 +0100

    callibration working for x and y now

--- inst/three/ui.R
+++ inst/three/ui.R
@@ -9,19 +9,17 @@ threeObjects <- getObjects('three')
 
 shinyUI(
   
-  
-  
-  fluidPage(
+  navbarPage(
     
-    # titlePanel("Hello Shiny!"),
+    #titlePanel("Hello Shiny!"),
     
-#    theme = shinyThemeToUse,
+    theme = shinyThemeToUse,
+    title = "polmineR",
+    id = "polmineR",
     
-#    title = "polmineR",
-    
-#    sidebarLayout(
+    tabPanel(
       
-#      "graph",
+      title = "graph",
       
       includeScript("/Library/Frameworks/R.framework/Versions/3.2/Resources/library/three/js/three.min.js"),
       includeScript("/Library/Frameworks/R.framework/Versions/3.2/Resources/library/three/js/TrackballControls.js"),
@@ -45,19 +43,34 @@ shinyUI(
           )
         )
       ),
+      
       tags$script("init()"), # happens on load
       tags$script("animate()"),
       
-      
-      # for making progress
+      # to pass calibration factors between slider inputs and javascript
       tags$script('var calibrationX = 5; Shiny.addCustomMessageHandler("passCallibrationX", function(calibrateX) {calibrationX = calibrateX; console.log(calibrationX);});'),
-      tags$script('var calibrationY = 5; Shiny.addCustomMessageHandler("passCallibrationY", function(calibrateY) {calibrationY = calibrateY; console.log(calibrationY);});'),
+      tags$script('var calibrationY = 5; Shiny.addCustomMessageHandler("passCallibrationY", function(calibrateY) {calibrationY = calibrateY; console.log(calibrationY);});')
       
       
-#    ),
-  
+    ),
+    
+    tabPanel(
+      
+      title = "settings",
+      
+      sidebarLayout(
+        sidebarPanel(
+          settingsUiInput()
+        ),
+        mainPanel(
+          
+        )
+        
+      )
+    ),
+    
     useShinyjs(debug = TRUE),
     extendShinyjs(script = "/Users/blaette/Lab/github/polmineR.graph/inst/three/www/shinyjs.js")
-
+    
   )
 )
